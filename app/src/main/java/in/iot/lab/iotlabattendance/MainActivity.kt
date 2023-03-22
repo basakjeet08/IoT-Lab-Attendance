389c10271520d54ce46b49e92d37b1e3338c49a5
Anirban Basak <jeetbasak2002@gmail.com>
2023-03-23 05:07:08 +0530

    added color Theme and Typography and all the theming stuffs

--- app/src/main/java/in/iot/lab/iotlabattendance/MainActivity.kt
+++ app/src/main/java/in/iot/lab/iotlabattendance/MainActivity.kt
@@ -4,25 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import `in`.iot.lab.iotlabattendance.core.theme.IoTLabAttendanceTheme
+import `in`.iot.lab.iotlabattendance.core.theme.CustomAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            IoTLabAttendanceTheme {
+            CustomAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
-
+                    // TODO
                 }
             }
         }
